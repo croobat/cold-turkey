54f173762def7bbfff6e5ea48c818f529f2b8fd4
Tony Ramírez <tonyramirez.business@outlook.com>
2024-12-20 21:56:08 -0600

    feat: setup paper provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,32 @@
 import { Stack } from 'expo-router';
+import { DarkTheme as NavigationDarkTheme, DefaultTheme as NavigationDefaultTheme } from '@react-navigation/native';
+import { MD3DarkTheme, MD3LightTheme, PaperProvider, adaptNavigationTheme } from 'react-native-paper';
+import merge from 'deepmerge';
+
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/utils/useColorScheme';
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+	reactNavigationLight: NavigationDefaultTheme,
+	reactNavigationDark: NavigationDarkTheme,
+});
+
+const customDarkTheme = { ...MD3DarkTheme, colors: Colors.dark };
+const customLightTheme = { ...MD3LightTheme, colors: Colors.light };
+
+const CombinedDefaultTheme = merge(LightTheme, customLightTheme);
+const CombinedDarkTheme = merge(DarkTheme, customDarkTheme);
 
 export default function RootLayout() {
-	return <Stack />;
+	const colorScheme = useColorScheme();
+
+	const theme = colorScheme === 'dark' ? CombinedDarkTheme : CombinedDefaultTheme;
+
+	return (
+		<PaperProvider theme={theme}>
+			<Stack>
+				<Stack.Screen name="index" options={{ headerShown: false }} />
+			</Stack>
+		</PaperProvider>
+	);
 }
