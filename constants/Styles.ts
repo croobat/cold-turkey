1e39e60a0979153a199dbf4e601d58772e83274d
Tony Ramírez <tonyramirez.business@outlook.com>
2024-12-21 15:13:06 -0600

    feat(constants): add style constants

--- constants/Styles.ts
+++ constants/Styles.ts
@@ -1,7 +1,12 @@
 import { StyleSheet } from 'react-native';
+import { METRICS } from './Metrics';
 
 export const style = StyleSheet.create({
 	centered: {
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	flexCentered: {
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
@@ -21,4 +26,20 @@ export const style = StyleSheet.create({
 		borderWidth: 1,
 		backgroundColor: 'lightred',
 	},
+
+	smallMargin: { margin: METRICS.small },
+	mediumMargin: { margin: METRICS.medium },
+	largeMargin: { margin: METRICS.large },
+
+	smallPadding: { padding: METRICS.small },
+	mediumPadding: { padding: METRICS.medium },
+	largePadding: { padding: METRICS.large },
+
+	smallRowGap: { rowGap: METRICS.small },
+	mediumRowGap: { rowGap: METRICS.medium },
+	largeRowGap: { rowGap: METRICS.large },
+
+	smallColumnGap: { columnGap: METRICS.small },
+	mediumColumnGap: { columnGap: METRICS.medium },
+	largeColumnGap: { columnGap: METRICS.large },
 });
