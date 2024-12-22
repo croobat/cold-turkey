c6d79737139c90c5ca374b41a6f1bf65969c81ef
Tony Ramírez <tonyramirez.business@outlook.com>
2024-12-21 18:00:33 -0600

    feat(home): add reset timer confirmation banner

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
-import { IconButton, Text } from 'react-native-paper';
+import { AnimatedFAB, Banner, IconButton, Portal, Text } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { router } from 'expo-router';
 
 import {} from '@/store/exampleSlice';
 import { useAppDispatch, useAppSelector } from '@/store';
@@ -9,9 +10,12 @@ import { selectLastQuote, updateLastQuote } from '@/store/motivationalSlice';
 
 import { style } from '@/constants/Styles';
 import motivationalQuotes from '@/constants/MotivationalQuotes.json';
+import { METRICS } from '@/constants/Metrics';
 
 export default function HomeScreen() {
 	const dispatch = useAppDispatch();
+
+	const [isResetConfirmVisible, setIsResetConfirmVisible] = useState(false);
 
 	const getRandomQuote = useCallback(() => {
 		const randomIndex = Math.floor(Math.random() * motivationalQuotes.length);
@@ -29,11 +33,36 @@ export default function HomeScreen() {
 
 	const lastQuote = useAppSelector(selectLastQuote);
 
+	const resetConfirmActions = [
+		{
+			label: 'Cancel',
+			onPress: () => setIsResetConfirmVisible(false),
+		},
+		{
+			label: 'Ok',
+			onPress: () => {
+				setIsResetConfirmVisible(false);
+				router.navigate('/home/reset');
+			},
+		},
+	];
+
 	return (
-		<ScrollView contentContainerStyle={style.flexCentered}>
-			<SafeAreaView style={[style.centered, style.largePadding]}>
+		<SafeAreaView style={[style.container, style.centered]}>
+			<ScrollView contentContainerStyle={style.flexCentered}>
+				<Portal>
+					<Banner
+						visible={isResetConfirmVisible}
+						icon="alert"
+						actions={resetConfirmActions}
+						style={{ paddingTop: METRICS.large }}
+					>
+						<Text variant="bodyLarge">Are you sure you want to reset the timer?</Text>
+					</Banner>
+				</Portal>
+
 				{/* motivational quote */}
-				<View style={[style.centered, style.smallRowGap]}>
+				<View style={[style.centered, style.smallRowGap, style.largeMargin, { maxWidth: 350 }]}>
 					<Text variant="bodyLarge" style={styles.quoteText}>
 						{lastQuote.quote}
 					</Text>
@@ -44,8 +73,16 @@ export default function HomeScreen() {
 
 					<IconButton icon="refresh" mode="contained" onPress={getRandomQuote} />
 				</View>
-			</SafeAreaView>
-		</ScrollView>
+			</ScrollView>
+
+			<AnimatedFAB
+				icon="restore"
+				label="Reset"
+				extended={false}
+				onPress={() => setIsResetConfirmVisible(true)}
+				style={style.fabStyle}
+			/>
+		</SafeAreaView>
 	);
 }
 
